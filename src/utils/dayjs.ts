906bd95dc6ed0bf7fa6a14ca9bed1c5e16c23455
baranwang <me@baran.wang>
2025-04-07 00:43:28 +0800

    chore: 更新 wrangler.jsonc 文件以修复 JSON 格式，注释掉不再使用的环境变量，并在 dayjs.ts 中添加 UTC 和时区插件以支持时间处理

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,8 +1,14 @@
 import dayjs from 'dayjs';
 import { PluginLunar } from 'dayjs-plugin-lunar';
 import 'dayjs/locale/zh-cn';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
 dayjs.locale('zh-cn');
 dayjs.extend(PluginLunar);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault('Asia/Shanghai');
 
 export { dayjs };
